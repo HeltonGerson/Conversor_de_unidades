00c55e3dce5aac568ce74f4a43d9f1202966f68c
Helton <heltongerson@alu.ufc.br>
2026-06-27 13:58:44 -0300

    Unidades de trabalho base (SIU)

---
+++ config.py
@@ -0,0 +1,26 @@
+unidades_comprimento = {
+    # Unidade base: metros
+    "km": 1000.0,
+    "m": 1.0,
+    "cm:": 0.01,
+    "mm": 0.001,
+}
+unidade_massa = {
+    # Unidade base: quilos
+    "t": 1000.0,
+    "kg": 1.0,
+    "g": 0.001,
+    "mg": 0.000001,
+}
+unidade_temperatura = {
+    # Unidade base: Kelvin.
+    "C": 1.0,
+    "K": 274.15,
+    "F": 33.8,
+}
+unidades_tempo = {
+    # unidade base: horas
+    "hr": 1,
+    "m": 60,
+    "s": 3600,
+}
